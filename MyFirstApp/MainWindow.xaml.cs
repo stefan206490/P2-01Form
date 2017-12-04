c4c9cd90ddb82baeceb3f9498de8a26c7898e926
Stefan Beckers <stefan.beckers206490@student.leeuwenborgh.nl>
2017-12-04 13:30:59 +0100

    get monster on screen

--- MyFirstApp/MainWindow.xaml.cs
+++ MyFirstApp/MainWindow.xaml.cs
@@ -20,18 +20,30 @@ namespace MyFirstApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int level;
         public MainWindow()
         {
             InitializeComponent();
             //comment
+            level = 0;
+            NoMonster();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void NoMonster()
         {
             Monster enemy = new Monster();
-            enemy.strMonType = "Rat";
-            enemy.intMonHP = 10;
-            enemy.intMonAttack = 2;
+            enemy.strMonType = "Montype: " + "geen";
+            enemy.intMonHP = 0;
+            enemy.intMonAttack = 0;
+        }
+
+        private void dungeon_Click(object sender, RoutedEventArgs e)
+        {
+            Monster enemy = new Monster();
+
+            MonType.Content = "Type: " + enemy.strMonType;
+            MonHP.Content = "HP: " + enemy.intMonHP;
+            MonAttack.Content = "Attack: " + enemy.intMonAttack;
         }
     }
     public class Monster
@@ -39,5 +51,29 @@ namespace MyFirstApp
         public string strMonType;
         public int intMonHP;
         public int intMonAttack;
+        public Monster()
+        {
+            //later wil ik meer monsters die ik vanuit hier met een random nummer aan kan spreken
+            //RanMon * floor level zo krijg ik voor elke floor level een x aantal monsters.
+            Random RanMon = new Random();
+            //voor elke depth 10 monsters alleen voor depth 0 ratten.
+            int MonIndx = MainWindow.level * RanMon.Next(1, 11); //creates a number between 1 and 10.
+            Monsterindex(MonIndx);
+        }
+        public void Monsterindex(int MonIndx)
+        {
+            switch(MonIndx)
+            {
+                case 0:
+                    Rat();
+                    break;
+            }
+        }
+        public void Rat()
+        {
+            this.strMonType = "Rat";
+            this.intMonHP = 10;
+            this.intMonAttack = 2;
+        }
     }
 }
